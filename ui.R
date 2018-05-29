b25a505711ad203dd0eae509f0b8e21a140dd1c5
Yiran Ni <yiran@D-10-19-176-93.dhcp4.washington.edu>
2018-05-29 15:51:45 -0700

    Add Analysis

--- ui.R
+++ ui.R
@@ -99,7 +99,31 @@ shinyUI(navbarPage(
   ),
 
   tabPanel(
-    "Analysis"
+    "Analysis",
+    headerPanel(
+      h1("Introduction and Data Analysis", align = "center")
+    ),
+    mainPanel(
+      p("This dataset a visualized way to understand the trend of US honey production throughout years."),
+      p("In Interactive 1, users is able to select and see the Total Production and Production Value of each state. From the oberservation, we found that 
+        the Total Production is decreasing while the Production Value is increasing from 1998 to 2012 in general. The following is the comparion of Total Production 
+        and Production Value in Florida."),
+      img(width=1000, height=500, src = "honey-production-FL.png", style = "margin-left:180px"),
+      p("In Interactive Plot 2, we take the average number of each variable from every state,
+        provided 5 national average variables related to honey production- Number of Colonies, Yield per Colony, Total Production, 
+        Price/ Pound, and Production Value."),
+      p("According to the observation, there is not too much change for the nuumber of colonies through the years."),
+      img(width=1000, height=500, src = "number-of-colonies.png", style = "margin-left:180px"),
+      p("The yield per colony has slightly decreased."),
+      img(width=1000, height=500, src = "yield-colony.png", style = "margin-left:180px"),
+      p("Compare to the decrease of yield per colony, the average total production has a greater decreasing especially from 
+        1998 to 2002. But the average total production is relatively constant after 2002."),
+      img(width=1000, height=500, src = "total-production.png", style = "margin-left:180px"),
+      p("The Price per Pound and Production Value have been increased a lot from 1998 to 2012, and reach the peaks in 2012."),
+      img(width=1000, height=500, src = "price-pound.png", style = "margin-left:180px"),
+      img(width=1000, height=500, src = "production-value.png", style = "margin-left:180px"),
+      width = 12
+      )
   ),
   tabPanel(
     "Table",
